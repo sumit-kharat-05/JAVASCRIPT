3b28bdaed0a48967a85087f332b1e8d7418ffed5
sumit-kharat-05 <sumitkharat686@gmail.com>
2026-04-26 12:52:08 +0530

    Javascript Lecture 1 Objects , Variables , Datatypes Etc

---
+++ script.js
@@ -0,0 +1,19 @@
+
+console.log("Hello, World!");
+const student ={
+    fullName : "Sumit kharat",
+    age : 22,
+    city : "Pune"
+}
+console.log(student);
+
+// Que1
+
+const product = {
+    tittle : "Ball Pen",
+    rating : 4.5,
+    isDealm : true,
+    price : 20,
+};
+
+console.log(product);
